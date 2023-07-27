22782aa83a866785de263cf986378649f5dd9cdc
manish <ms5978307@gmail.com>
2023-07-27 13:36:00 +0530

    fixed autherization

--- src/main/java/com/manish/security/controller/ProductController.java
+++ src/main/java/com/manish/security/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.manish.security.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,19 @@ public class ProductController {
     }
 
     @GetMapping("/student")
+    @PreAuthorize("hasRole('STUDENT')")
     public String getProductStudent(){
         return "This should be visible to only student";
     }
 
     @GetMapping("/teacher")
+    @PreAuthorize("hasRole('TEACHER')")
     public String getProductTeacher(){
         return "This should be visible to only teacher";
     }
 
     @GetMapping("/management")
+    @PreAuthorize("hasRole('MANAGEMENT')")
     public String getProductManagement(){
         return "This should be visible to only management";
     }
